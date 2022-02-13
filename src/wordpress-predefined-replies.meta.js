55e5d0ce0daf5e03d7e1d0a38745e9bae70a4b36
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2022-02-13 01:41:33 +0100

    Create wordpress-predefined-replies.meta.js

---
+++ src/wordpress-predefined-replies.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         WordPress.org Predefined Replies
+// @namespace    https:/wordpress.org/
+// @version      0.1
+// @description  Add saved replies to topic reply forms.
+// @author       Scott Kingsley Clark, Clorith
+// @match        https://wordpress.org/support/topic/*
+// @match        https://*.wordpress.org/support/topic/*
+// @updateURL    https://github.com/wporg-support/predefined-replies/raw/main/src/wordpress-predefined-replies.user.js
+// @downloadURL  https://github.com/wporg-support/predefined-replies/raw/main/src/wordpress-predefined-replies.user.js
+// @grant        GM_setValue
+// @grant        GM_getValue
+// ==/UserScript==
