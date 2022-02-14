6640a5c8f98896408d2310ec47ef2e95f321933a
Yui ★ღツ ゆい <43382763+fierevere@users.noreply.github.com>
2022-02-14 16:39:17 +0300

    indendation for ru.json, revert local url changes for upstream pull request

--- src/wordpress-predefined-replies.meta.js
+++ src/wordpress-predefined-replies.meta.js
@@ -6,8 +6,8 @@
 // @author       Scott Kingsley Clark, Clorith
 // @match        https://wordpress.org/support/topic/*
 // @match        https://*.wordpress.org/support/topic/*
-// @updateURL    https://github.com/fierevere/predefined-replies/raw/main/src/wordpress-predefined-replies.user.js
-// @downloadURL  https://github.com/fierevere/predefined-replies/raw/main/src/wordpress-predefined-replies.user.js
+// @updateURL    https://github.com/wporg-support/predefined-replies/raw/main/src/wordpress-predefined-replies.user.js
+// @downloadURL  https://github.com/wporg-support/predefined-replies/raw/main/src/wordpress-predefined-replies.user.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // ==/UserScript==
